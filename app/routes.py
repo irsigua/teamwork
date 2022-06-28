8f78d9f1f0de716b6301c52c205f850b44995190
Irakliy Sigua <irsigua@yandex.ru>
2022-06-28 22:32:10 +0300

    view\route

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*- 
-from flask import render_template, flash, redirect, url_for
+import profile
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
+import requests
+import csv
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'User'}
     return render_template('index.html', title='Home', user=user)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -16,3 +21,10 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    request_info = request.files['file']
+    
+
