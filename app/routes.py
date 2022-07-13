bde33934ad83b9d96222a51375993f4dc50b68ed
Sam Fedosenko <Co0mrad@yandex.ru>
2022-07-14 00:50:17 +0500

    3

--- app/routes.py
+++ app/routes.py
@@ -19,16 +19,16 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 @app.route('/data',methods = ['GET'])
 def data():
-    companys = Company.query.order_by(Company.inn).all()
-    companys_dict={}
-    for company in companys:
-        companys_dict['inn'] = company.inn
-        companys_dict['name'] = company.name
-    directions = Direction.query.order_by(Direction.set_number).all()
-    directions_dict={}
-    for direction in directions:
-        directions_dict['set_number'] = direction.set_number
-        directions_dict['payer'] = direction.payer
-        directions_dict['recipient'] = direction.recipient
-    print(companys_dict)    
-    return render_template('data.html',companys=companys,directions=directions)
+    companys = Company.query.join(Direction,Company.inn==Direction.company_inn).with_entities(Company.inn,Company.name,Direction.set_number,Direction.payer,Direction.recipient).all()
+    companys_list = []
+    for inn, name, set_number, payer, recipient in companys:
+        companys_dict={}
+        companys_dict['inn'] = inn
+        companys_dict['name'] = name
+        companys_dict['set_number'] = set_number
+        companys_dict['payer'] = payer
+        companys_dict['recipient'] = recipient
+        companys_list.append(companys_dict)
+    print(companys_list)          
+    # return render_template('data.html',companys=companys)
+    return None
